88527f95720740715f5433052612a1752e78ee36
t-mine <tomonori.mine.biz@gmail.com>
2025-03-08 21:25:11 +0900

    ポモドーロタイマーとして最低限機能するようにした

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,73 +1,94 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useTimer } from 'react-timer-hook';
 
 interface PomodoroState {
   mode: 'work' | 'break';
   completedCount: number;
+  isPaused: boolean;
 }
 
 interface SettingsState {
   workDuration: number;
   breakDuration: number;
   goalPomodoros: number;
+  isAutoStart: boolean;
 }
 
-// Timerコンポーネント
 const Timer: React.FC = () => {
 
-  // 設定ステート
+  // setting state
   const [setting, setSetting] = useState<SettingsState>({
     workDuration: 10,
     breakDuration: 5,
-    goalPomodoros: 2
+    goalPomodoros: 2,
+    isAutoStart: false
   });
 
-  // タイマーステート
+  // pomodor state
   const [pomodoroState, setPomodoroState] = useState<PomodoroState>({
     mode: 'work',
     completedCount: 0,
+    isPaused: false
   });
 
+  // 作業時間 or 休憩時間を取得する
   const getTimerDuration = () => {
     return pomodoroState.mode === "work"
       ? setting.workDuration 
       : setting.breakDuration
   };
-  const getExpiryDateFromDuration = (durationSeconds: number): Date => {
+
+  // react-timer-hook用のタイマー終了時間を算出
+  const getExpiryDateFromDuration = (durationSeconds : number): Date => {
     return new Date(Date.now() + durationSeconds * 1000);
   };
+
   const onExpire = () => {
+    // 作業終了の場合
     if (pomodoroState.mode === "work") {
-      const newSetCount = pomodoroState.completedCount + 1;
-      setPomodoroState((prevState)=>{
-        return {
-          ...prevState,
-          mode: "break",
-          completedCount: newSetCount
-        }
-      });
-      restart(getExpiryDateFromDuration(setting.breakDuration), false);
+      const completedCount = pomodoroState.completedCount + 1;
+      // 目標ポモドーロ数に達している場合、達成ポモドーロ数のみ更新
+      if (completedCount === setting.goalPomodoros) {
+        setPomodoroState((prevState)=>({...prevState, completedCount: completedCount}));
+        return;
+      }
+      // 休憩開始
+      setPomodoroState((prevState)=>({...prevState, mode: "break", completedCount: completedCount}));
+      setTimeout(() => restart(getExpiryDateFromDuration(setting.breakDuration), setting.isAutoStart),1);
     } else {
-      setPomodoroState((prevState)=>{
-        return {
-          ...prevState,
-          mode: "work",
-        }
-      });
-      restart(getExpiryDateFromDuration(setting.workDuration), false);
+      // 休憩終了の場合
+      // 作業開始
+      setPomodoroState((prevState) => ({ ...prevState, mode: "work" }));
+      setTimeout(() => restart(getExpiryDateFromDuration(setting.workDuration), setting.isAutoStart),1);
     }
   };
 
-  const { seconds, minutes, isRunning, start, pause, restart } = useTimer({
+  // useTimer
+  const { seconds, minutes, isRunning, start, pause, restart, resume } = useTimer({
     expiryTimestamp: getExpiryDateFromDuration(getTimerDuration()),
     autoStart: false,
-    onExpire: () => onExpire(),
+    onExpire: onExpire,
   });
 
-  const resetTimer = () => {
+  // onReset
+  const onReset = () => {
+    setPomodoroState((prevState)=>({...prevState, mode: "work", completedCount: 0}));
     restart(getExpiryDateFromDuration(setting.workDuration), false)
   }
 
+  // onPause
+  const onPause = () => {
+    pomodoroState.isPaused = true;
+    pause();
+  }
+
+  // onResume
+  const onResume = () => {
+    pomodoroState.isPaused = false;
+    resume();
+  }
+
+  // format time
   const formatTime = (m: number, s: number) => {
     const mm = m.toString().padStart(2, "0");
     const ss = s.toString().padStart(2, "0");
@@ -77,24 +98,26 @@ const Timer: React.FC = () => {
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center bg-gray-800">
       <div className="text-center">
-        {/* 時間 */}
+        {/* time */}
         <div className="text-8xl font-bold mb-4 text-white">{formatTime(minutes, seconds)}</div>
-        {/* ボタン */}
+        {/* buttons */}
         <div className="space-x-2">
           {!isRunning ? (
-            <button onClick={start} className="bg-teal-700 text-white w-[7.5rem] px-4 py-2 rounded">
-              start
+            <button onClick={pomodoroState.isPaused ? onResume : start} 
+              className="bg-teal-700 text-white w-[7.5rem] px-4 py-2 rounded disabled:bg-gray-600 disabled:cursor-not-allowed" 
+              disabled={pomodoroState.completedCount === setting.goalPomodoros}>
+              {pomodoroState.isPaused ? "resume" : "start"}
             </button>
           ) : (
-            <button onClick={pause} className="bg-amber-700 text-white w-[7.5rem] px-4 py-2 rounded">
+            <button onClick={onPause} className="bg-amber-700 text-white w-[7.5rem] px-4 py-2 rounded">
               stop
             </button>
           )}
-          <button onClick={resetTimer} className="bg-rose-700 text-white w-[7.5rem] px-4 py-2 rounded">
+          <button onClick={onReset} className="bg-rose-700 text-white w-[7.5rem] px-4 py-2 rounded">
             reset
           </button>
         </div>
-        {/* 完了ポモドーロ数 */}
+        {/* completed */}
         <div className="mt-4 text-white">completed : {pomodoroState.completedCount} / {setting.goalPomodoros}</div>
       </div>
     </div>
