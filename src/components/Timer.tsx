7ad7ce60d785bd55d55e58db052fa2d56344492f
t-mine <tomonori.mine.biz@gmail.com>
2025-03-26 21:02:19 +0900

    fix

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -9,7 +9,6 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
-import { Switch } from "@/components/ui/switch";
 import Radio from "@/components/ui/radio";
 
 interface PomodoroState {
